d2811dd587c6cffe3fa58089c86fc9531823a8c5
janghun <ytre4386@naver.com>
2020-01-31 00:16:15 +0900

    메이븐주소바꿈

--- AriWeb/src/kr/co/ari/main/web/MainController.java
+++ AriWeb/src/kr/co/ari/main/web/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 
-	@RequestMapping("ari/main1")
+	@RequestMapping("/")
 	public String MainPage() {
 		
 		
@@ -35,5 +35,4 @@ public class MainController {
 		
 		return "/cmmn/dataAccessFailure";
 	}
-	
 }
